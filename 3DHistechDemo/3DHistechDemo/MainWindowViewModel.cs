6b87534c6e5f8f627f7e4ed3d74e37dbd5f8aa97
JakabAndi <jakab.andi2@gmail.com>
2023-12-13 17:58:09 +0100

    Create the project and the classes

---
+++ 3DHistechDemo/3DHistechDemo/MainWindowViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3DHistechDemo
+{
+    internal class MainWindowViewModel
+    {
+        private List<IEngine> engineList;
+        private ITable myTable;
+
+        internal MainWindowViewModel(List<IEngine> engines, ITable table)
+        {
+            engineList = engines;
+            myTable = table;
+        }
+
+        
+    }
+}
